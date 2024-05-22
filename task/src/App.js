1be3e667c55691fe931d64cae2e4308abb8aaf3b
Tajas8340 <pawartejajas8340@gmail.com>
2024-05-22 13:14:50 +0530

    add

---
+++ task/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Login from './auth/Login';
+import Register from './auth/Register';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      <Register />
+    </div>
+  );
+}
+
+export default App;
